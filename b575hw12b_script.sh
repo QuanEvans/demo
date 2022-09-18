e14c923597e8a45d5363ca1da8a4b6ef2f246625
QuanEvans <18670351591@163.com>
2022-09-18 19:10:58 -0400

    adding hw1 sh file

---
+++ b575hw12b_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "#==SCRIPT STARTED"
+
+awk '{if (NR%4==2) print$0}' sample.fastq > sample_rseq.txt
+awk '{if (NR%4==2) print$0}' sample_cutadapt.fastq > sample_cutadapt_rseq.txt
+
+echo "#== RETRIEVE READ SEQUENCE DONE"
+
+sed 's/AGATCGGAAGAGC.*//' sample_rseq.txt > sample_rseq_CUT.txt
+
+echo "#== CUT ADAPTER DONE"
+
+diff sample_rseq_CUT.txt sample_cutadapt_rseq.txt
+
+echo "#== SCRIPT DONE"
